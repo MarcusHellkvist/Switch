02105fe7ec90abdbcfeb5ef25fb09eb892d8f37b
ajizi <hellqvistmarcus@gmail.com>
2017-04-23 20:52:05 +0200

    Added shit
    sprites and doors

---
+++ Switch/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+
+	private PlayerController player;
+	public GameObject doorDestination;
+
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.name == "Player") {
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				Debug.Log ("DOOR ACTIVE!");
+				player.transform.position = doorDestination.transform.position;
+			}
+		}
+	}
+
+}
